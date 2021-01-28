4f0324a13bcdd8ac3e08907be95169828510dae2
TrevMike <preince560m@gmail.com>
2021-01-27 20:14:27 -0600

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -14,22 +14,53 @@ import "./App.css";
 function App() {
   const [isAuth, setIsAuth] = useState(false);
 
-  useEffect(() => {
-    axios
-      .post(`http://localhost:5000/auth/is_verified`, {
-        header: { token: localStorage.token },
-      })
-      .then((res) => {
-        const parseRes = res.text();
-        console.log(parseRes);
-        // parseRes === true ? setIsAuth(true) : setIsAuth(false);
-        // console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
+  // useEffect(() => {
+  //   axios
+  //     .post(`http://localhost:5000/auth/is_verified`, {
+  //       header: { jwt_token: localStorage.token },
+  //     })
+  //     .then((res) => {
+  //       const parseRes = res.json();
+  //       console.log(parseRes);
+  //       parseRes === true ? setIsAuth(true) : setIsAuth(false);
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+  // async function authChecker(){
+  //       axios
+  //     .post(`http://localhost:5000/auth/is_verified`, {
+  //       header: { jwt_token: localStorage.token },
+  //     })
+  //     .then((res) => {
+  //       const parseRes = await res.json();
+  //       console.log(parseRes);
+  //       parseRes === true ? setIsAuth(true) : setIsAuth(false);
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }
+  const checkAuthenticated = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/auth/is_verified", {
+        method: "POST",
+        headers: { token: localStorage.token },
       });
-  }, []);
 
+      const parseRes = await res.json();
+
+      parseRes === true ? setIsAuth(true) : setIsAuth(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
   const setAuth = (toggle) => {
     setIsAuth(toggle);
   };
